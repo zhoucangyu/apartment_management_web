a482b217540b9cef9d64cc4ee0d124094359202c
zhoucangyu <luanzcy@163.com>
2019-01-27 14:51:17 +0800

    spring boot 初始文件

---
+++ src/main/java/com/me/apartment_management_web/ApartmentManagementWebApplication.java
@@ -0,0 +1,14 @@
+package com.me.apartment_management_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentManagementWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartmentManagementWebApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/me/apartment_management_web/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.me.apartment_management_web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApartmentManagementWebApplication.class);
+    }
+
+}
+
